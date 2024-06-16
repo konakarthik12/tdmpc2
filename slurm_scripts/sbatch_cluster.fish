738912ddde7441f7c5144e55c7b73cab99d171f0
konakarthik12 <koankarthik12@gmail.com>
2024-06-16 11:49:20 -0400

    changes

--- slurm_scripts/sbatch_cluster.fish
+++ slurm_scripts/sbatch_cluster.fish
@@ -1,7 +1,6 @@
 #!/nfshomes/kkona/local/bin/fish
-set slurm_dir $argv[1]
-mkdir -p $slurm_dir
-cd $slurm_dir
+mkdir -p $SLURM_WORK_DIR
+cd $SLURM_WORK_DIR
 
 echo "Starting job..."
 echo "with job id: $SLURM_JOB_ID"
@@ -18,8 +17,6 @@ echo "Repo url: $repo_url"
 echo "Commit hash: $commit_hash"
 echo "Overrides: $argv"
 
-. $OG_PATH/setup_cluster.fish $slurm_dir
-
 mkdir tdmpc2
 
 git -C tdmpc2 init
@@ -27,3 +24,4 @@ git -C tdmpc2 remote add origin $repo_url
 git -C tdmpc2 fetch --depth 1 origin $commit_hash
 git -C tdmpc2 checkout FETCH_HEAD
 
+. tdmpc2/setup_cluster.fish .
