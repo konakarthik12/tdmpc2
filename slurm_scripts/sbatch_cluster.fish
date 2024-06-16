72e04bfef6c226afbb6fa759dd3f3bbd496be34b
konakarthik12 <koankarthik12@gmail.com>
2024-06-16 11:37:22 -0400

    changes

--- slurm_scripts/sbatch_cluster.fish
+++ slurm_scripts/sbatch_cluster.fish
@@ -1,15 +1,15 @@
 #!/nfshomes/kkona/local/bin/fish
 
-cd (dirname (status -f))
-
+set slurm_dir $SCRATCH_DIR/slurm_runs/$SLURM_JOB_ID/
+mkdir -p $slurm_dir
+cd $slurm_dir
 
 echo "Starting job..."
 echo "with job id: $SLURM_JOB_ID"
 echo "with args: $argv"
 echo "Running on $(hostname)"
+echo "In directory: $(pwd)"
 
-set slurm_dir $SCRATCH_DIR/slurm_runs/$SLURM_JOB_ID/
-mkdir -p $slurm_dir
 nvidia-smi
 
 set repo_url $argv[1]
@@ -19,9 +19,7 @@ echo "Repo url: $repo_url"
 echo "Commit hash: $commit_hash"
 echo "Overrides: $argv"
 
-. setup_cluster.fish $slurm_dir
-
-cd $slurm_dir
+. setup_cluster.fish .
 
 mkdir tdmpc2
 
@@ -30,4 +28,3 @@ git -C tdmpc2 remote add origin $repo_url
 git -C tdmpc2 fetch --depth 1 origin $commit_hash
 git -C tdmpc2 checkout FETCH_HEAD
 
-echo $slurm_dir
