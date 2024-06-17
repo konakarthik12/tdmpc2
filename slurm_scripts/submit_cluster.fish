7cddff095f3721d0cd10a55e2fa0b83a9b566c87
konakarthik12 <koankarthik12@gmail.com>
2024-06-16 22:00:27 -0400

    changes

--- slurm_scripts/submit_cluster.fish
+++ slurm_scripts/submit_cluster.fish
@@ -2,9 +2,15 @@
 
 cd (dirname (status -f))
 . ./utils.fish
-. /fs/nexus-projects/KGB-MBRL/scratch_hold/init_scratch_dir.fish
-. /fs/nexus-projects/KGB-MBRL/scratch_hold/setup_conda.fish
+if ! set task_name (python3 extract_task_name.py $argv)
+    . /fs/nexus-projects/KGB-MBRL/scratch_hold/init_scratch_dir.fish
+    . /fs/nexus-projects/KGB-MBRL/scratch_hold/setup_conda.fish
+end
 
+if ! set task_name (python3 extract_task_name.py $argv)
+    echo "Failed to extract task name"
+    exit 1
+end
 set repo_url (git remote get-url origin)
 
 if ! set commit_hash (verify_changes_pushed)
@@ -21,10 +27,7 @@ if test (count $argv) -le 1
     exit 1
 end
 
-if ! set task_name (python3 extract_task_name.py $argv)
-    echo "Failed to extract task name"
-    exit 1
-end
+
 set full_name "tdmpc2-$task_name"
 sbatch --job-name="$full_name" sbatch_cluster.sh $repo_url $commit_hash $argv
 echo "Job submitted"
