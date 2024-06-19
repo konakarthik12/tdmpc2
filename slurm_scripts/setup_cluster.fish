c0701549b40f86fdbdcb29a7c684a8bb155d1d8d
konakarthik12 <koankarthik12@gmail.com>
2024-06-19 14:36:46 -0400

    changes

--- slurm_scripts/setup_cluster.fish
+++ slurm_scripts/setup_cluster.fish
@@ -1,14 +1,13 @@
 #!/usr/bin/env fish
 
 echo "Setting up cluster in $(realpath $argv[1])"
-. /fs/nexus-projects/KGB-MBRL/scratch_hold/setup_all.fish $argv[1]
+# . /fs/nexus-projects/KGB-MBRL/scratch_hold/setup_all.fish $argv[1]
 
 pushd (dirname (status -f))
 cd ..
 
+echo "Generating tdmpc2 conda environment"
 conda env update -n tdmpc2 -f environment.yaml -q
 
-
-conda run -n tdmpc2 pip install "git+https://github.com/StanfordVL/OmniGibson@v1.0.0#egg=omnigibson" -q
-conda run -n tdmpc2 pip install -r tdmpc2_requirements.txt -q
 popd
+echo "Cluster is ready to run tdmpc2"
